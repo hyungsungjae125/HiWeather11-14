d547819a575b029fb3d0e7cd04126da634e3f6fa
형성재 <tldwo125@naver.com>
2018-11-15 12:09:34 +0900

    날씨 클래스 날씨상태 클래스 생성

---
+++ WindowsFormsHiWeather/WindowsFormsHiWeather/Conditions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsHiWeather
+{
+    class Conditions
+    {
+        string condition;//현재상태
+        int temperature;//온도
+        int humidity;//습도
+        int dust;//미세먼지농도
+        int windSpeed;//풍속
+        string windDirection;//풍향
+
+        //일별 날씨 상태에 대해서 사용하는 생성자
+        public Conditions(string condition,int temperature)
+        {
+            this.condition = condition;
+            this.temperature = temperature;
+        }
+
+        //시간별 날씨 상태에 대해서 사용하는 생성자
+        public Conditions(string condition, int temperature,int humidity,int dust,int windSpeed,string windDirection)
+        {
+            this.condition = condition;
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.dust = dust;
+            this.windSpeed = windSpeed;
+            this.windDirection = windDirection;
+        }
+
+        public string Condition { get => condition; }
+        public int Temperature { get => temperature; }
+        public int Humidity { get => humidity; }
+        public int Dust { get => dust; }
+        public int WindSpeed { get => windSpeed; }
+        public string WindDirection { get => windDirection; }
+    }
+}

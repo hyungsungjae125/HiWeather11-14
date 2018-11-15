d1cde6abe1a001688b4faa87c6e2efe442dc351f
지현 이 <ljh5432@gmail.com>
2018-11-15 10:37:30 +0900

    main

--- WindowsFormsHiWeather/WindowsFormsHiWeather/Program.cs
+++ WindowsFormsHiWeather/WindowsFormsHiWeather/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsHiWeather
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_config());
+            Application.Run(new Main());
         }
     }
 }
